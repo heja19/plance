7858539db059bb4473f1f022976a4e1192ff0597
osvaldk <kokla.osvald@gmail.com>
2021-10-07 13:37:49 +0300

    Start integrating Vue into main app

---
+++ fbaseapp/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/Screenshot_6.png">
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
